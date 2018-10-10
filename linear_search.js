3a8e2b33331aad0509af75a5d5ff1bc983bee9b8
George Babayan <ggbabayan@gmail.com>
2018-10-10 11:55:15 +0300

    Update linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -1,15 +1,1 @@
-function linearSearch(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) 
-      return i
-  }
-}
 
-function globalSearch(array, number) {
-  let arrayAnswer=[]
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) 
-      answer.push(i)
-  }
-  return arrayAnswer
-}
