1e51b23845ff63aceacce771266ac6666d7c3699
Leksa <52825447+LeksaGirl@users.noreply.github.com>
2019-08-14 14:20:44 +0300

    update

--- spec/linear_search_spec.js
+++ spec/linear_search_spec.js
@@ -1,1 +1,7 @@
-
+describe("linearSearch()", function() {
+  it("returns first index of item in array", function() {
+    let item = 3;
+    let arr = [1,2,3,4,5];
+    expect(linearSearch(item, arr)).toEqual(2);
+  });
+});
