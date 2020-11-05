0a803a520cc0e705829ab366a9f5b138db7c41f8
Roman Paramzin <filpoyma@gmail.com>
2020-11-05 18:17:10 +0300

    Update linear_search_spec.js

--- spec/linear_search_spec.js
+++ spec/linear_search_spec.js
@@ -5,3 +5,11 @@ describe("linearSearch()", function() {
     expect(linearSearch(item, arr)).toEqual(2);
   });
 });
+
+describe("globalSearch()", function() {
+  it("returns array indxes of item in array", function() {
+    let item = 4;
+    let arr = [1,1,2,3,4,4,5];
+    expect(globalSearch(item, arr)).toEqual(2);
+  });
+});
