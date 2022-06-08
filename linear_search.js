9b44fe4525954c92ea730b5426333d7c96b42207
Alex <petenkov2822@gmail.com>
2022-06-08 17:15:51 +0300

    release 0-1

--- linear_search.js
+++ linear_search.js
@@ -1,11 +1,28 @@
 //Реализуй функции линейного поиска. Проверь результат тестами.
 
-function linearSearch() {
-  
-}
+function linearSearch(item, arr) {
+  // for (let i = 0; i < arr.length; i++) {
+  //   if(item === arr[i]) {
+  //     return i
+  //   }
+  // }   //// или
+  let res;
+  arr.map((el, i) => {
+    if (el === item) {
+      res = i;
+    }
+  })
+  return res;
+};
 
-function globalLinearSearch() {
-  
+function globalLinearSearch(item, arr) {
+  let res = [];
+  arr.map((el, i) => {
+    if(el === item) {
+      res.push(i);
+    }
+  })
+  return res;
 }
 
 module.exports = {
